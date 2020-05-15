1620b4c3cfaba6b1769d73145094ca95b059dcfb
Weiquan Huang <47123863+weiquanh@users.noreply.github.com>
2020-05-15 15:07:59 -0700

    Add files via upload

---
+++ studentdatabaseapp/Student.java
@@ -0,0 +1,98 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String classStanding;
+	private String studentID;
+	private String courses = "";
+	private double tuitionBalance;
+	private static int courseCost = 600;
+	
+	// constructor: prompt to enter name and year of each student
+	public Student() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Student's First Name: ");
+		this.firstName = input.nextLine();
+		
+		System.out.print("Enter Student's Last Name: ");
+		this.lastName = input.nextLine();
+		
+		System.out.print("1) Freshman \n2) Sophmore \n3) Junior \n4) Senior"
+				+ "\nEnter Student's Class Standing(choose number above): ");
+		switch(input.nextInt()) {
+			case 1:
+				this.classStanding = "Freshman";
+				break;
+			case 2:
+				this.classStanding = "Sophmore";
+				break;
+			case 3:
+				this.classStanding = "Junior";
+				break;
+			case 4:
+				this.classStanding = "Senior";
+				break;
+		}
+		
+		setStudentID();
+		
+//		System.out.println("Student " + firstName + " " + lastName + " - Grade " 
+//				+ year + " - SID " + studentID);
+	}
+	
+	// generate random 7-digit student ID
+	private void setStudentID() {
+		String studentIDSet = "0123456789";
+    	char[] studentID = new char[7];
+    	for (int i = 0; i < studentID.length; i++) {
+    		int rand = (int) (Math.random() * studentIDSet.length());
+    		studentID[i] = studentIDSet.charAt(rand);
+    	}
+    	this.studentID =  new String(studentID);
+	}
+	
+	// enroll courses
+	public void enroll() {
+		while (true) {
+			System.out.print("Enter A Course To Enroll(Q to quit): ");
+			Scanner input = new Scanner(System.in);
+			String course = input.nextLine();
+			if (!course.equals("Q")) {
+				courses = courses + "\n	" + course;
+				tuitionBalance += courseCost;
+			} else {
+				break;
+			}
+		}
+//		System.out.println("Student Enrolled In: " + courses);
+//		System.out.println("Tuition Balance: " + tuitionBalance);
+	}
+	
+	// view balance
+	public void viewBalance() {
+		System.out.println("Current Tuition Balance is $" + tuitionBalance);
+	} 
+	
+	// pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter the Amount to pay: $");
+		Scanner input = new Scanner(System.in);
+		double payment = input.nextDouble();
+		tuitionBalance -= payment;
+		System.out.println("Thank You for the Payment of $" + payment);
+		viewBalance();
+	}
+	
+	// see status
+	public void displayInfo() {
+		System.out.println("Student Name: " + firstName + " " + lastName +
+				"\nClass Standing: " + classStanding +
+				"\nStudentID: " + studentID +
+				"\nCourses Enrolled: " + courses + 
+				"\nTuition Balance: $" + tuitionBalance);
+	}
+}
---
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,47 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+/*
+Scenario: You are a Database Administrator for a university and need to
+		  create an application to manage student enrollments and balance.
+The application should do the following:
+1. Ask the user how many new students will be added to the database
+2. The user should be prompted to enter the name and year for each student
+3. The student should have a 7-digit random ID
+4. A student can enroll in the following courses:
+   History 101
+   Mathematics 101
+   English 101
+   Chemistry 101
+   Computer Science 101
+5. Each course costs $600 to enroll
+6. The student should be able to view their balance and pay the tuition
+7. To see the status of the student, we should see their name, ID, 
+   courses enrolled, and balance
+*/
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ask the user how many new students will be added to the database
+		System.out.print("Enter number of new students to enroll: ");
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		
+		// create n number of new students
+		Student[] students = new Student[number];
+		for (int i = 0; i < number; i++) {
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].payTuition();
+		}
+		
+		// display all entered students' info
+		for (int i = 0; i < number; i++) {
+			students[i].displayInfo();
+		}
+	}
+
+}
