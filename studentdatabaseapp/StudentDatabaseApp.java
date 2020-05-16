19fc6c125088cf6eedc9c0d2b46534a6d41a386a
Weiquan Huang <47123863+weiquanh@users.noreply.github.com>
2020-05-15 17:05:38 -0700

    Update StudentDatabaseApp.java

--- studentdatabaseapp/StudentDatabaseApp.java
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -3,22 +3,23 @@ package studentdatabaseapp;
 import java.util.Scanner;
 
 /*
-Scenario: You are a Database Administrator for a university and need to
-		  create an application to manage student enrollments and balance.
+Scenario: You are a Database Administrator for a university and need to create an application 
+	  to manage student enrollments and balance.
+
 The application should do the following:
 1. Ask the user how many new students will be added to the database
-2. The user should be prompted to enter the name and year for each student
-3. The student should have a 7-digit random ID
-4. A student can enroll in the following courses:
+2. The user should be prompted to enter the name and class standing for each student
+3. The student should have a 7-digit random student ID
+4. A student can enroll in the following courses, for example, 
    History 101
    Mathematics 101
    English 101
    Chemistry 101
    Computer Science 101
 5. Each course costs $600 to enroll
-6. The student should be able to view their balance and pay the tuition
-7. To see the status of the student, we should see their name, ID, 
-   courses enrolled, and balance
+6. The student should be able to view their tuition balance and pay the tuition
+7. To see the status of the student, we should see their name, student ID, courses enrolled, 
+   and tuition balance
 */
 
 public class StudentDatabaseApp {
